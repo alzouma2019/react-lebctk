fbb465d23f8769401d5678639be1e4356229a9d9
alzouma2019 <57457748+alzouma2019@users.noreply.github.com>
2022-10-06 12:44:34 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com/posts',
+});
 
 export default function App() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    async function getPost() {
+      const response = await client.get('/1');
+      setPost(response.data);
+    }
+    getPost;
+  }, []);
+
+  async function deletePost() {
+    await client.delete('/1');
+    alert('Post deleted!');
+    setPost(null);
+  }
+
+  if (!post) return 'No post!';
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      <button onClick={deletePost}>Delete Post </button>
     </div>
   );
 }
